b1d1a7adf2d8e8957a8dce7191697dfd0767d08b
davidmueller13 <david.mueller1013@gmail.com>
2015-12-19 21:29:40 +0100

    fix compiling

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1066,7 +1066,7 @@ static ssize_t show_target_loads(
 			       i & 0x1 ? ":" : " ");
 #endif
 
-	ret += sprintf(buf + --ret, "\n");
+	ret = target_loads[i];
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
@@ -1135,7 +1135,7 @@ static ssize_t show_above_hispeed_delay(
 			       i & 0x1 ? ":" : " ");
 #endif
 
-	ret += sprintf(buf + --ret, "\n");
+	ret = target_loads[i];
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
 	return ret;
 }
