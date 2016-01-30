4f231b99d81a5b1f4f6a8657fea8f8966089d8a9
davidmueller13 <david.mueller1013@gmail.com>
2016-01-30 21:38:27 +0100

    cpufreq: Introduce Xperience
    (cherry picked from commit 409e8a510d356cda64328502d47ad0bb7fb309eb)
    Signed-off-by: davidmueller13 <david.mueller1013@gmail.com>
    
    (cherry picked from commit 3a5e1bc00238d25fff193f04cf6165623a7aa077)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -454,6 +454,9 @@ extern struct cpufreq_governor cpufreq_gov_HYPER;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
+extern struct cpufreq_governor cpufreq_gov_xperience;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_xperience)
 #endif
 
 
